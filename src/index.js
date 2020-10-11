2c83182e96d6b69bbe7a0515d3b4b464841838ca
Josh Fielding <josh.e.fielding@gmail.com>
2020-10-12 08:02:55 +1100

    added play pause button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import style from "./sass/index.scss";
 import "bootstrap";
 import fontawesome from "@fortawesome/fontawesome-free";
-
 import video from "./assets/videos/radio-254-compressed.mp4";
+
+const playBtn = document.querySelector(".fa-play");
+const stopBtn = document.querySelector(".fa-stop");
+const audio = document.getElementById("audio");
+
+function play() {
+  audio.play();
+  playBtn.classList.add("inactive");
+  stopBtn.classList.add("active");
+}
+
+playBtn.addEventListener("click", () => {
+  play();
+});
+
+stopBtn.addEventListener("click", () => {
+  audio.pause();
+  playBtn.classList.add("active");
+  playBtn.classList.remove("inactive");
+  stopBtn.classList.remove("active");
+});
