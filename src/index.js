bb543643367e68ccb6267dc6191d8ba0772aad39
Josh Fielding <josh.e.fielding@gmail.com>
2020-10-16 08:05:58 +1100

    added posters

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "bootstrap";
 import fontawesome from "@fortawesome/fontawesome-free";
 import video from "./assets/videos/logo-cutdown-compressed.mp4";
 
+
+// Playbutton
 const playBtn = document.querySelector(".fa-play");
 const stopBtn = document.querySelector(".fa-stop");
 const audio = document.getElementById("audio");
@@ -21,6 +23,8 @@ stopBtn.addEventListener("click", () => {
   stopBtn.classList.remove("active");
 });
 
+
+// Sidebar
 const sideBar = document.querySelector(".sidebar");
 const title = document.querySelector(".title");
 const content = document.querySelector(".content");
@@ -30,3 +34,28 @@ sideBar.addEventListener("click", () => {
   title.classList.toggle("isHidden");
   content.classList.toggle("isContentActive");
 });
+
+
+
+// Text loop
+const text2Loop = [
+  "a platform and non-profit",
+  "showcasing Kenyan musicians, creatives and curatives.",
+];
+
+textSequence(0);
+
+function textSequence(i) {
+  const textAnimate = document.querySelector(".text-animate");
+  if (textAnimate !== null) {
+    if (text2Loop.length > i) {
+      setTimeout(function() {
+        textAnimate.innerHTML = text2Loop[i];
+        textSequence(++i);
+      }, 3000); // 1 second (in milliseconds)
+    } else if (text2Loop.length == i) {
+      // Loop
+      textSequence(0);
+    }
+  }
+}
