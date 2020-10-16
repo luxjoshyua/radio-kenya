6297657fe02cd82667c361b98ac5f71ac1dd44b7
luxjoshyua <josh.e.fielding@gmail.com>
2020-10-17 09:18:19 +1100

    added hamburger nav

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import "bootstrap";
 import fontawesome from "@fortawesome/fontawesome-free";
 import video from "./assets/videos/logo-cutdown-compressed.mp4";
 
+console.log(
+    "%c Built by josh, reach out at josh.e.fielding@gmail.com",
+    "font-size: 30px;background:blue;text-shadow:5px 5px 0 green"
+);
+
 // Playbutton
 const playBtn = document.querySelector(".fa-play");
 const stopBtn = document.querySelector(".fa-stop");
 const audio = document.getElementById("audio");
 
 playBtn.addEventListener("click", (event) => {
-  // console.log("e is registering", event);
-  audio.play();
-  playBtn.classList.add("inactive");
-  stopBtn.classList.add("active");
+    // console.log("e is registering", event);
+    audio.play();
+    playBtn.classList.add("inactive");
+    stopBtn.classList.add("active");
 });
 
 stopBtn.addEventListener("click", () => {
-  audio.pause();
-  playBtn.classList.add("active");
-  playBtn.classList.remove("inactive");
-  stopBtn.classList.remove("active");
+    audio.pause();
+    playBtn.classList.add("active");
+    playBtn.classList.remove("inactive");
+    stopBtn.classList.remove("active");
 });
 
 // Sidebar
@@ -27,40 +32,53 @@ const sideBar = document.querySelector(".sidebar");
 const title = document.querySelector(".title");
 const content = document.querySelector(".content");
 sideBar.addEventListener("click", () => {
-  sideBar.classList.toggle("isActive");
-  sideBar.firstElementChild.classList.toggle("isActive");
-  title.classList.toggle("isHidden");
-  content.classList.toggle("isContentActive");
+    sideBar.classList.toggle("isActive");
+    sideBar.firstElementChild.classList.toggle("isActive");
+    title.classList.toggle("isHidden");
+    content.classList.toggle("isContentActive");
 });
 
 // Text loop
 const text2Loop = [
-  "a platform and non-profit",
-  "showcasing Kenyan musicians, creatives and curatives.",
+    "a platform and non-profit",
+    "showcasing Kenyan musicians, creatives and curatives.",
 ];
 
 textSequence(0);
 
 function textSequence(i) {
-  const textAnimate = document.querySelector(".text-animate");
-  if (textAnimate !== null) {
-    if (text2Loop.length > i) {
-      setTimeout(function () {
-        textAnimate.innerHTML = text2Loop[i];
-        textSequence(++i);
-      }, 3000); // 1 second (in milliseconds)
-    } else if (text2Loop.length == i) {
-      // Loop
-      textSequence(0);
+    const textAnimate = document.querySelector(".text-animate");
+    if (textAnimate !== null) {
+        if (text2Loop.length > i) {
+            setTimeout(function () {
+                textAnimate.innerHTML = text2Loop[i];
+                textSequence(++i);
+            }, 3000); // 1 second (in milliseconds)
+        } else if (text2Loop.length == i) {
+            // Loop
+            textSequence(0);
+        }
     }
-  }
 }
 
 // Hamburger Nav
-const hamburger = document.querySelector(".hamburger");
-const navContentMb = document.querySelector(".nav-links-mb");
-console.log(hamburger);
-hamburger.addEventListener("click", (e) => {
-  console.log(e);
-  navContentMb.classList.toggle("activeNavMb");
-});
+const hamburger = document.querySelector(".hamburger-nav");
+const navDom = document.querySelector(".nav-links-mb");
+// const navLinks = document.querySelectorAll(".sticky-overlay .inner a");
+if (hamburger !== null) {
+    hamburger.addEventListener("click", function () {
+        toggleNav();
+    });
+}
+// if (navLinks !== null) {
+//   navLinks.forEach(link => {
+//     link.addEventListener("click", function() {
+//       toggleNav();
+//     });
+//   });
+// }
+
+function toggleNav() {
+    hamburger.classList.toggle("active");
+    navDom.classList.toggle("activeNavMb");
+}
