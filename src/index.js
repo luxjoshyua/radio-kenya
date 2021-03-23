9405338f08e4954ab7d172848cfc974a827df062
luxjoshyua <josh.e.fielding@gmail.com>
2021-03-23 14:40:40 +1100

    added api integration

--- src/index.js
+++ src/index.js
@@ -1,65 +1,65 @@
-import style from "./sass/index.scss";
-import "bootstrap";
-import fontawesome from "@fortawesome/fontawesome-free";
-import video from "./assets/videos/logo-cutdown-compressed.mp4";
+import style from './sass/index.scss';
+import 'bootstrap';
+import fontawesome from '@fortawesome/fontawesome-free';
+import video from './assets/videos/logo-cutdown-compressed.mp4';
+import './radio-api';
 
-import poster1 from "./assets/images/poster1-tiny.jpg";
-import poster2 from "./assets/images/poster2-tiny.jpg";
-import poster3 from "./assets/images/poster3-tiny.jpg";
-import poster4 from "./assets/images/poster.jpg";
-import favicon from "./assets/favicon/favicon.png";
+import poster1 from './assets/images/poster1-tiny.jpg';
+import poster2 from './assets/images/poster2-tiny.jpg';
+import poster3 from './assets/images/poster3-tiny.jpg';
+import poster4 from './assets/images/poster.jpg';
+import favicon from './assets/favicon/favicon.png';
 // console.log(favicon);
 
-import robot from "./docs/robots.txt";
+import robot from './docs/robots.txt';
 // console.log(robot);
-import sitemap from "./docs/sitemap.xml";
+import sitemap from './docs/sitemap.xml';
 // console.log(sitemap);
 
 console.log(
-  "%c Built by josh, reach out at josh.e.fielding@gmail.com",
-  "font-size: 30px;background:blue;text-shadow:5px 5px 0 green"
+  '%c Built by josh, reach out at josh.e.fielding@gmail.com',
+  'font-size: 30px;background:blue;text-shadow:5px 5px 0 green'
 );
 
 // Playbutton
-const playBtn = document.querySelector(".fa-play");
-const stopBtn = document.querySelector(".fa-stop");
-const audio = document.getElementById("audio");
+const playBtn = document.querySelector('.fa-play');
+const stopBtn = document.querySelector('.fa-stop');
+const audio = document.getElementById('audio');
 
-playBtn.addEventListener("click", (event) => {
-  // console.log("e is registering", event);
+playBtn.addEventListener('click', (event) => {
   audio.play();
-  playBtn.classList.add("inactive");
-  stopBtn.classList.add("active");
+  playBtn.classList.add('inactive');
+  stopBtn.classList.add('active');
 });
 
-stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
   audio.pause();
-  playBtn.classList.add("active");
-  playBtn.classList.remove("inactive");
-  stopBtn.classList.remove("active");
+  playBtn.classList.add('active');
+  playBtn.classList.remove('inactive');
+  stopBtn.classList.remove('active');
 });
 
 // Sidebar
-const sideBar = document.querySelector(".sidebar");
-const title = document.querySelector(".title");
-const content = document.querySelector(".content");
-sideBar.addEventListener("click", () => {
-  sideBar.classList.toggle("isActive");
-  sideBar.firstElementChild.classList.toggle("isActive");
-  title.classList.toggle("isHidden");
-  content.classList.toggle("isContentActive");
+const sideBar = document.querySelector('.sidebar');
+const title = document.querySelector('.title');
+const content = document.querySelector('.content');
+sideBar.addEventListener('click', () => {
+  sideBar.classList.toggle('isActive');
+  sideBar.firstElementChild.classList.toggle('isActive');
+  title.classList.toggle('isHidden');
+  content.classList.toggle('isContentActive');
 });
 
 // Text loop
 const text2Loop = [
-  "A platform and non-profit",
-  "showcasing Kenyan musicians, creatives and curatives.",
+  'A platform and non-profit',
+  'showcasing Kenyan musicians, creatives and curatives.',
 ];
 
 textSequence(0);
 
 function textSequence(i) {
-  const textAnimate = document.querySelector(".text-animate");
+  const textAnimate = document.querySelector('.text-animate');
   if (textAnimate !== null) {
     if (text2Loop.length > i) {
       setTimeout(function () {
@@ -74,40 +74,29 @@ function textSequence(i) {
 }
 
 // Hamburger Nav
-const hamburger = document.querySelector(".hamburger-nav");
-const navDom = document.querySelector(".nav-links-mb");
+const hamburger = document.querySelector('.hamburger-nav');
+const navDom = document.querySelector('.nav-links-mb');
 // const navLinks = document.querySelectorAll(".sticky-overlay .inner a");
 if (hamburger !== null) {
-  hamburger.addEventListener("click", function () {
+  hamburger.addEventListener('click', function () {
     toggleNav();
   });
 }
 
 function toggleNav() {
-  hamburger.classList.toggle("active");
-  navDom.classList.toggle("activeNavMb");
+  hamburger.classList.toggle('active');
+  navDom.classList.toggle('activeNavMb');
 }
-
-// Marquee
-function makeMarquee() {
-  const title = "Playing 100% Kenyan content 24/7";
-  const marqueeText = new Array(80).fill(title).join(" — ");
-  const marquee = document.querySelector(".marquee-wrapper span");
-  marquee.innerHTML = marqueeText;
-}
-
-// here we actually run our makeMarquee function
-makeMarquee();
 
 // Random poster
 setInterval(() => {
   const imageArray = [
-    "./img/poster1-tiny.jpg",
-    "./img/poster2-tiny.jpg",
-    "./img/poster3-tiny.jpg",
+    './img/poster1-tiny.jpg',
+    './img/poster2-tiny.jpg',
+    './img/poster3-tiny.jpg',
   ];
   const randomImageItem =
     imageArray[Math.floor(Math.random() * imageArray.length)];
-  const posterImage = document.querySelector(".poster-wrapper img");
+  const posterImage = document.querySelector('.poster-wrapper img');
   posterImage.src = randomImageItem;
 }, 600000);
---
+++ src/radio-api.js
@@ -0,0 +1,23 @@
+const apiURL = 'https://public.radio.co/stations/sba1f00abf/history';
+let data = [];
+
+fetch(apiURL)
+  .then((data) => {
+    return data.json();
+  })
+  .then((result) => {
+    data = result;
+    setCurrentSong(data);
+  })
+  .catch((error) => {
+    console.log('here is API error = ', error);
+  });
+
+const setCurrentSong = (data) => {
+  const songSpan = document.getElementById('song-title');
+  const currentSong = (songSpan.innerHTML = data.tracks[0].title);
+  const title = 'Now playing: ';
+  const marqueeText = new Array(80).fill(title).join(`${currentSong} - `);
+  const marquee = document.querySelector('.marquee-wrapper .intro-text');
+  marquee.innerHTML = marqueeText;
+};
