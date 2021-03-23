19e6c9cf5b61bd7f4190900ffdb843084f6619d7
luxjoshyua <josh.e.fielding@gmail.com>
2021-03-23 14:43:02 +1100

    slowed down marquee

--- src/radio-api.js
+++ src/radio-api.js
@@ -17,7 +17,7 @@ const setCurrentSong = (data) => {
   const songSpan = document.getElementById('song-title');
   const currentSong = (songSpan.innerHTML = data.tracks[0].title);
   const title = 'Now playing: ';
-  const marqueeText = new Array(80).fill(title).join(`${currentSong} - `);
+  const marqueeText = new Array(40).fill(title).join(`${currentSong} - `);
   const marquee = document.querySelector('.marquee-wrapper .intro-text');
   marquee.innerHTML = marqueeText;
 };
